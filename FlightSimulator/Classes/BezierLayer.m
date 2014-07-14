8aaf27eee27268d506fb4e95b62c7f7f5ebad47e
gongpengjun <frank.gongpengjun@gmail.com>
2014-07-14 22:49:48 +0800

    添加可以触摸的BezierLayer

--- FlightSimulator/Classes/BezierLayer.m
+++ FlightSimulator/Classes/BezierLayer.m
@@ -7,7 +7,15 @@
 //
 
 #import "BezierLayer.h"
+#import "Plane.h"
 
+@interface BezierLayer ()
+{
+    ccBezierConfig bezierConfig;
+    CCActionBezierTo *bezierAction;
+    Plane *plane;
+}
+@end
 
 @implementation BezierLayer
 
@@ -22,9 +30,26 @@
 	if(self)
     {
         self.contentSize = [CCDirector sharedDirector].designSize;
+        self.userInteractionEnabled = YES;
+        plane = [Plane plane];
+        plane.position = ccp(self.contentSize.width/2, self.contentSize.height/2);
+        [self addChild:plane];
     }
     return self;
 }
 
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    CGPoint touchLoc = [touch locationInNode:self];
+    CCLOG(@"%s,%d %@",__FUNCTION__,__LINE__,NSStringFromCGPoint(touchLoc));
+    
+    bezierConfig.controlPoint_1 = ccp(100,100);
+    bezierConfig.controlPoint_2 = ccp(100,-100);
+    bezierConfig.endPosition = touchLoc;
+    
+    bezierAction = [CCActionBezierTo actionWithDuration:1.0f bezier:bezierConfig];
+    [plane runAction:bezierAction];
+}
 
 @end
