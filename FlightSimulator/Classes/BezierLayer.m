29cecddb1b3fad8eccda2e357df66d7795f6cdc7
gongpengjun <frank.gongpengjun@gmail.com>
2014-07-15 12:24:06 +0800

    让飞机沿着两个平滑的曲线飞行

--- FlightSimulator/Classes/BezierLayer.m
+++ FlightSimulator/Classes/BezierLayer.m
@@ -11,9 +11,7 @@
 
 @interface BezierLayer ()
 {
-    ccBezierConfig bezierConfig;
-    CCActionBezierTo *bezierAction;
-    Plane *plane;
+    Plane *_plane;
 }
 @end
 
@@ -31,9 +29,9 @@
     {
         self.contentSize = [CCDirector sharedDirector].designSize;
         self.userInteractionEnabled = YES;
-        plane = [Plane plane];
-        plane.position = ccp(self.contentSize.width/2, self.contentSize.height/2);
-        [self addChild:plane];
+        _plane = [Plane plane];
+        _plane.position = ccp(self.contentSize.width/2, self.contentSize.height/2);
+        [self addChild:_plane];
     }
     return self;
 }
@@ -41,15 +39,42 @@
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
+    
     CGPoint touchLoc = [touch locationInNode:self];
     CCLOG(@"%s,%d %@",__FUNCTION__,__LINE__,NSStringFromCGPoint(touchLoc));
     
+    ccBezierConfig bezierConfig;
+#if 0
     bezierConfig.controlPoint_1 = ccp(100,100);
     bezierConfig.controlPoint_2 = ccp(100,-100);
     bezierConfig.endPosition = touchLoc;
+    CCActionBezierTo *bezierAction = [CCActionBezierTo actionWithDuration:1.0f bezier:bezierConfig];
+    [_plane runAction:bezierAction];
+#elif 0
+    CGPoint curLoc = _plane.position;
+    bezierConfig.controlPoint_1 = ccp(curLoc.x,touchLoc.y);
+    bezierConfig.controlPoint_2 = touchLoc;
+    bezierConfig.endPosition = touchLoc;
+    CCActionBezierTo *bezierAction = [CCActionBezierTo actionWithDuration:1.0f bezier:bezierConfig];
+    [_plane runAction:bezierAction];
+#else
+    CGPoint curLoc = _plane.position;
+    CGPoint centerLoc = ccpMidpoint(curLoc, touchLoc);
     
-    bezierAction = [CCActionBezierTo actionWithDuration:1.0f bezier:bezierConfig];
-    [plane runAction:bezierAction];
+    bezierConfig.controlPoint_1 = ccp(centerLoc.x,curLoc.y);
+    bezierConfig.controlPoint_2 = centerLoc;
+    bezierConfig.endPosition = centerLoc;
+    CCActionBezierTo *bezierAction1 = [CCActionBezierTo actionWithDuration:1.0f bezier:bezierConfig];
+    
+    bezierConfig.controlPoint_1 = ccp(centerLoc.x,touchLoc.y);
+    bezierConfig.controlPoint_2 = touchLoc;
+    bezierConfig.endPosition = touchLoc;
+    CCActionBezierTo *bezierAction2 = [CCActionBezierTo actionWithDuration:1.0f bezier:bezierConfig];
+    
+    CCActionSequence *seq = [CCActionSequence actionOne:bezierAction1 two:bezierAction2];
+    [_plane runAction:seq];
+#endif
+    
 }
 
 @end
